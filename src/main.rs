0705736295618503b6c7ac3f9bda8e97ede0c451
coffeeknife <coffeeknife@proton.me>
2024-12-12 10:51:40 -0600

    basic structure for detecting subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "TodoApp")]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {     
+    },
+    Done {
+    },
+    Del {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Add {}) => {
+            println!("Add subcommand found!")
+        },
+        Some(Commands::Del {}) => {
+            println!("Del subcommand found!")
+        },
+        Some(Commands::Done {}) => {
+            println!("Done subcommand found!")
+        }
+        None => {
+            println!("Main program (print todo list)")
+        }
+    }
 }
