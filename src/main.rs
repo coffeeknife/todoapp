f022b366a8c514c47af7314fafae2a45adde621c
coffeeknife <coffeeknife@proton.me>
2024-12-12 11:16:03 -0600

    fill in args

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,42 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Add {     
+    Add {
+        #[arg(value_delimiter=' ', num_args=1.., required=true)]
+        items: Option<Vec<String>>
     },
     Done {
+        #[arg(value_delimiter=' ', num_args=1.., required=true)]
+        ids: Option<Vec<u64>>
     },
     Del {
+        #[arg(value_delimiter=' ', num_args=1.., required=true)]
+        ids: Option<Vec<u64>>
+    },
+    Print {
+        #[arg(short, long, conflicts_with="full")]
+        done: bool,
+        #[arg(short, long, conflicts_with="done")]
+        full: bool,
+        #[arg(value_delimiter=' ', num_args=1..)]
+        ids: Option<Vec<u64>>
     }
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Some(Commands::Add {}) => {
+        Some(Commands::Add { items }) => {
             println!("Add subcommand found!")
-        },
-        Some(Commands::Del {}) => {
+        }
+        Some(Commands::Del { ids }) => {
             println!("Del subcommand found!")
-        },
-        Some(Commands::Done {}) => {
+        }
+        Some(Commands::Done { ids }) => {
             println!("Done subcommand found!")
+        }
+        Some(Commands::Print { done, full, ids }) => {
+            println!("Print subcommand found! Done: {done}, Full: {full}");
         }
         None => {
             println!("Main program (print todo list)")
